298b0d4ff1f66a7c6c6a085240148336c581b48e
Shadow141206@gmail.com <Shadow141206@gmail.com@13b2789b-b7af-12d3-a9ee-b806c932aa63>
2011-11-10 09:29:24 +0000

    GUI (bản kiểm thử) File functional req, business rule v.v... bản đầu tiên

---
+++ trunk/Requirement Management/Software Requirement Specification/[HRM] GUI/[HRM] GUI/LoginPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace _HRM__GUI
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
